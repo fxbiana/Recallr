76a145c58c9162f8878c1911c3806b6a38bae3f5
fabianalozada <fabiana.lozadaperdomo@gmail.com>
2025-02-10 13:28:13 -0800

    Initial Commit

---
+++ Recallr/RecallrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallrApp.swift
+//  Recallr
+//
+//  Created by Fabiana Lozada on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RecallrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
